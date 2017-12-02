12dd3c56c5f8df39f277e45d1e56ff16899d6b61
Avery Durrant <avery246813579@gmail.com>
2017-12-01 16:06:36 -0800

    Started Map Integration

---
+++ src/co/frostbyte/jge/map/Map.java
@@ -0,0 +1,11 @@
+package co.frostbyte.jge.map;
+
+import co.frostbyte.jge.entities.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Map {
+    private List<Entity> entities = new ArrayList<>();
+
+}
